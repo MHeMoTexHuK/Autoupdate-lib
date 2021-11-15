fb7a6b61a800197152f8f24548620cf702a45b09
MHeMoTexHuK <daytimer253@gmail.com>
2021-11-15 21:59:06 +0300

    i just want to test whether that'll work

---
+++ src/io/mnemotechnican/autoupdater/Updater.java
@@ -0,0 +1,14 @@
+package io.mnemotechnician.autoupdater;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class Updater {
+	
+	public static final prefix = "#!VERSION";
+	
+	public static void checkUpdates(Mod mod) {
+		
+	}
+	
+}
