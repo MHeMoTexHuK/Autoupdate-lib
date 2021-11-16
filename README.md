de92d9805120de408124be78328f2bd67b0f7f5c
MHeMoTexHuK <daytimer253@gmail.com>
2021-11-16 19:48:52 +0300

    idk whether it'll work

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
+## Autoupdate lib
+Allows any mindustry mods to easily check for updates.
+
+## Usage
+The jar must be added to the mod as an implementation dependency in order for it to work.
+
+Specify the following lines in your mod.(h)json file:
 ```
-Brazil       You
-  ^           /
-   \_________/
+#!VERSION integer_version;
+#!REPO author/repo;
 ```
+example:
+```
+#!VERSION 24;
+#!REPO MHeMoTexHuK/New-controls;
+```
+
+Call Updater.checkUpdates(currentMod) after client load and it'll check whether the repo has a newer version.
+If it has, it'll prompt the user to automatically update it and will download & install it.
+
+TODO: upload the artifact to maven or smth?
+
+##
--- src/io/mnemotechnican/autoupdater/Updater.java
+++ src/io/mnemotechnican/autoupdater/Updater.java
@@ -1,14 +1,143 @@
 package io.mnemotechnician.autoupdater;
 
 import arc.util.*;
+import arc.files.*;
+import arc.util.io.*;
+import mindustry.*;
 import mindustry.mod.*;
 
 public class Updater {
 	
-	public static final prefix = "#!VERSION";
+	public static final String url = "https://github.com/", urlFile= "/blob/master/", urlDownload = "/archive/master.zip";
+	//used for comparison. I can't just compare String and StringBuilder without toString()-ing
+	public static final StringBuilder prefixVersion = new StringBuilder("VERSION"), prefixRepo = new StringBuilder("REPO");
+	//temporary builder
+	public static final StringBuilder check = new StringBuilder();
 	
-	public static void checkUpdates(Mod mod) {
+	protected static String repo;
+	protected static int version;
+	
+	public static void checkUpdates(Mod originMod) {
+		var mod = Vars.mods.getMod(originMod);
+		var file = mod.file;
 		
+		Fi meta = null;
+		if (mod.isDirectory()) {
+			meta = getMeta(file);
+		} else if (file == null) {
+			Log.err("Failed to check updates for: " + mod.name);
+			return;
+		} else {
+			meta = getMeta(new ZipFi(file));
+		}
+		
+		if (meta == null) {
+			Log.err("Unable to locate mod info file for: " + mod.name);
+			return;
+		}
+		
+		if (!checkInfo(meta)) {
+			return;
+		}
+		int currentVersion = version;
+		String currentRepo = repo;
+		
+		//todo: retry upon a non-fatal error?
+		Http.get(url + currentRepo + urlEnd + meta.name(), response -> {
+			Fi newMeta = Fi.tempFile("meta"); //unfortunately.
+			newMeta.writeBytes(response.getResult());
+			
+			if (!checkInfo(newMeta)) {
+				return;
+			}
+			
+			if (currentVersion < version) {
+				Vars.ui.showCustomConfirm(
+					"Update available",
+					"New version of " + mod.name + " available!",
+					"[green]Update",
+					"[red]Not now",
+					
+					() -> Http.get(url + currentRepo + urlDownload)
+					.error(e -> Vars.ui.showException("Download failed!"), e)
+					.submit(response -> {
+						file.writeBytes(response.getResult());
+						Vars.ui.showInfo("Mod " + mod.name + "had been updated succefully. Restart the game to apply changes.");
+					}),
+					
+					() -> {}
+				);
+			}
+		});
+	}
+	
+	public static Fi getMeta(Fi root) {
+		return root.child("mod.hjson").exists() ? root.child("mod.hjson")
+			: root.child("mod.json").exists() ? root.child("mod.json") : null;
+	}
+	
+	/** Reads the providen meta-info file and, unless an error occurs, outputs the version & repo to the respective fields of this class */
+	protected static boolean checkInfo(Fi meta) {
+		boolean rfound = false, vfound = false;
+		Reads reads;
+		try {
+			reads = meta.reads();
+			byte b;
+			
+			global:
+			while (!(rfound && vfound)) {
+				if (reads.input.available() < 6) { //6 is the min num of characters for any token (#!A B;)
+					return false;
+				}
+				
+				//skip to the next #! mark. ##! will be ignored, that's intended.
+				while (reads.b() != '#' || reads.b() != '!');
+				
+				//read control token
+				check.setLength(0);
+				while ((b = reads.b()) != ' ') check.append((char) b);
+				
+				if (check.compareTo(prefixVersion)) {
+					check.setLength(0);
+					while ((b = reads.b()) != ';' && b != '\n') {
+						if (!Character.isDigit((char) b)) {
+							Log.warn("Version must be an integer and can only contain digits, skipping");
+							continue global;
+						}
+						check.append((char) b);
+					}
+					//internal version only contains digits, thus no exceptions will be thrown... i hope.
+					version = Integer.valueOf(check.toString());
+					vfound = true;
+				} else if (check.compareTo(prefixRepo)) {
+					check.setLength(0);
+					while ((b = reads.b()) != ';' && b != '\n') check.append((char) b);
+					
+					if (check.indexOf("/") < 1) {
+						Log.warn("Repo must be in format of AUTHOR/REPOSITY_NAME, skipping");
+						continue global;
+					}
+					
+					repo = check.toString();
+					rfound = true;
+				} else {
+					Log.warn("Unknown control token: #!" + check);
+					continue;
+				}
+				
+			}
+			
+			return true;
+		} catch (java.io.EOFException e) {
+			Log.err("No repo/version specified in " + meta.name() + " (unexpected EOF)");
+			return false;
+		} catch (Exception e) {
+			Log.err("Exception occurred while reading mod info: " + meta.name(), e);
+			reads.close();
+			return false;
+		} finally {
+			reads.close();
+		}
 	}
 	
 }
